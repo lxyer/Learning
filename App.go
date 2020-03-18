46266342c1c4f3d1d5767a59d1b646e5fe93b9d8
lixiangyang <lixiangyang@qianxin.com>
2020-03-18 19:21:26 +0800

    update

--- App.go
+++ App.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	//声明变量
 	fmt.Print("Hello Word!")
 	//动态推断类型
 	var a int = 10
 	var b = 10
 	//可以省略var的写法
-	c :=10
-	fmt.Print(a+b+c)
+	c := 10
+	fmt.Print(a + b + c)
+	g, h := 10, "lxyer"
+	fmt.Print(add(2, 4))
+	fmt.Print(swap(string(g), h))
 
+}
+func add(num1, num2 int) int {
+	return num1 + num2
+}
+
+func swap(str1, str2 string) (string, string) {
+	return str2, str1
 }
